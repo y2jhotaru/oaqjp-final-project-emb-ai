a12f1b8a0084a82d948e95c339cb91a5fc616349
y2jhotaru <jorlandors@gmail.com>
2026-04-08 22:03:19 -0400

    Static code analysis

--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+''' This is the main file of the Emotion Detector Application 
+    executed over the Flask channel and deployed on
+    localhost:5000.
+'''
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,11 +9,15 @@ app = Flask("Emotion Detection")
 
 @app.route("/emotionDetector")
 def emot_detector():
+    ''' This function receives the text from the HTML interface
+        and runs the emotion detector function to return the
+        score of all the evaluated emotions and the dominant one
+    '''
     text_to_analyze = request.args.get('textToAnalyze')
 
     response = emotion_detector(text_to_analyze)
 
-    if response['dominant_emotion'] == None:
+    if response['dominant_emotion'] is None:
         return "Invalid text! Please try again!."
 
     formatted_result = (
@@ -26,6 +34,9 @@ def emot_detector():
 
 @app.route("/")
 def render_index_page():
+    '''
+        This function renders the main page using Flask
+    '''
     return render_template ("index.html")
 
 if __name__ == "__main__":
